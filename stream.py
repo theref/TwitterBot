e3e28957b20848f19d9688d2b7673dd2368b3779
James Campbell <james.campbell@tanti.org.uk>
2018-09-23 15:05:33 +0100

    Add logging and error handling to stream

--- stream.py
+++ stream.py
@@ -1,6 +1,10 @@
 from tweepy import OAuthHandler, Stream, StreamListener
+import daiquiri
+import logging
 import settings
 
+daiquiri.setup(level=logging.INFO)
+logger = daiquiri.getLogger()
 products = settings.products
 
 
@@ -9,15 +13,21 @@ def parse_tweet(text):
     """
     if "Buy" in text:
         side = "Buy"
-    else:
+    elif "Sell" in text:
         side = "Sell"
+    else:
+        return False
 
+    product = False
     for p in products:
         if p in text:
             product = p
             break
 
-    return {"side": side, "product": product}
+    if product:
+        return {"side": side, "product": product}
+    else:
+        return False
 
 
 class CustomStreamListener(StreamListener):
@@ -26,27 +36,36 @@ class CustomStreamListener(StreamListener):
             text = status.text
             if "trigger" in text:
                 data = parse_tweet(text)
-                print(data)
+                if data:
+                    logger.info(
+                        f"Received tweet: {data['side']} {data['product']}"
+                    )
+                else:
+                    logger.warning(f"Could not parse tweet: {text}")
         except Exception as e:
-            # log exception
-            pass
+            logger.error(e)
 
     def on_error(self, status_code):
-        # print >> sys.stderr, 'Encountered error with status code:', status_code
+        logger.error(f"Encountered error with status code: {status_code}")
         return True  # Don't kill the stream
 
     def on_timeout(self):
-        # print >> sys.stderr, 'Timeout...'
+        logger.warning("Timeout")
         return True  # Don't kill the stream
 
 
-def main():
+def build_stream():
     listener = CustomStreamListener()
     auth = OAuthHandler(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET)
     auth.set_access_token(
         settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET
     )
     stream = Stream(auth, listener)
+    return stream
+
+
+def main():
+    stream = build_stream()
     stream.filter(follow=["960163880768102400"])
 
 
